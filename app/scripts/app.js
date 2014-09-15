674b6eb27e05f46568ddb45e3210b587c7720a5e
Lucio Poveda <lucio.poveda@gmail.com>
2014-09-15 16:41:28 +0000

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name workspaceApp
+ * @description
+ * # workspaceApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('workspaceApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
